66c3ebe016b6b8ea222f5a544d71b81d827c8b6b
Valerie <119544006+WEDN3SDAY@users.noreply.github.com>
2023-01-10 19:05:36 -0500

    Full website recode // no more 1 file index

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+    <meta content="gloved.site" property="og:title"/>
+    <meta content="personal website for all my stuff" property="og:description"/>
+    
+     <link rel="stylesheet" href="style.css">
+
+     <title>gloved.site</title>
+
+     <body>
+
+    
+       <center>
+          <h1 style="opacity: 1.5; color: rgb(128,28,228); font-family: monospace">gloved.site</h1>
+          <br>
+          <br>
+          <a style="text=size:18; opacity 1.5; color: rgb(128,28,228); font-family: monospace" onmousedown="discord.open():> > Discord < </a>
+          <a style="text=size:18; opacity 1.5; color: rgb(128,28,228); font-family: monospace">|</a>
+          <a style="text=size:18; opacity 1.5; color: rgb(128,28,228); font-family: monospace" onmousedown="projects.open():>  Projects < </a>
+
+       </center>
+</body>
+
+<script>
+   function discord.open() {
+    window.open("discord.gg/CkSFfQhH");
+   }
+    
+
+   function projects.open() {
+    window.open("projects");
+   }
+
+</script>
+</html>
